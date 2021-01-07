7cc8c1a3a211f37590dfd4040fb3b730aba67866
masutaniR <popohu87@gmail.com>
2021-01-07 06:14:25 +0000

    [Fix] 4-10 プロフィール画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '54183c072f75e80d81182414bd0b0102ee99963a9267584019eb23ca6ad7d5dd23b3ae2895897591065d104e098c64211c5f6ed8c80da0cdeffa932dcd4d3722'
